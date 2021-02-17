8c8b8c95c1d40778bc42a54dc506bb073149df4e
outdead <outdead@mail.ru>
2021-02-17 04:30:45 +0300

    Add tls_handshake_timeout to http client config

--- internal/utils/httputil/httputil.go
+++ internal/utils/httputil/httputil.go
@@ -13,8 +13,9 @@ const DefaultTimeout = 10 * time.Second
 
 // Config contains configuration to net/http client wrapper.
 type Config struct {
-	Timeout time.Duration `json:"timeout" yaml:"timeout"`
-	Delay   time.Duration `json:"delay" yaml:"yaml"`
+	Timeout             time.Duration `json:"timeout" yaml:"timeout"`
+	TLSHandshakeTimeout time.Duration `json:"tls_handshake_timeout" yaml:"tls_handshake_timeout"`
+	Delay               time.Duration `json:"delay" yaml:"yaml"`
 }
 
 // A Client is wrapper for net/http with dial and requests timeouts.
@@ -25,13 +26,18 @@ type Client struct {
 
 // NewClient creates new Client instance.
 func NewClient(cfg *Config) *Client {
+	tlsHandshakeTimeout := cfg.Timeout
+	if cfg.TLSHandshakeTimeout != 0 {
+		tlsHandshakeTimeout = cfg.TLSHandshakeTimeout
+	}
+
 	c := Client{
 		Config: cfg,
 		Client: http.Client{
 			Timeout: cfg.Timeout,
 			Transport: &http.Transport{
 				Dial:                (&net.Dialer{Timeout: cfg.Timeout}).Dial,
-				TLSHandshakeTimeout: cfg.Timeout,
+				TLSHandshakeTimeout: tlsHandshakeTimeout,
 			},
 		},
 	}
